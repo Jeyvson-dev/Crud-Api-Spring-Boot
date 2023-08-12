4427b9970e01adaec4d301af8a59298d2e7a6b20
Jeyvson Moreira <jeyvson0000@gmail.com>
2023-08-12 16:14:07 -0300

    Refactor Controller

--- src/main/java/com/startup/startup/controller/PersonController.java
+++ src/main/java/com/startup/startup/controller/PersonController.java
@@ -2,6 +2,10 @@ package com.startup.startup.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.startup.startup.model.Person;
@@ -20,33 +24,33 @@ public class PersonController {
     @Autowired
     private PersonServices services;
 
-    @RequestMapping(method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person createPerson(@RequestBody Person person){
 
         return services.createPerson(person);
     } 
 
-    @RequestMapping(method=RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List findAll(){
 
         return services.findAll();
 
     }
 
-    @RequestMapping(method=RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person updatePerson(@RequestBody Person person){
 
         return services.updatePerson(person);   
     }
 
-    @RequestMapping(value = "{id}",method=RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public String deletePerson(@PathVariable(value = "id") Long id){
 
         return services.deletePerson(id);
 
     } 
 
-    @RequestMapping(value = "{id}",method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") Long id){
 
         return services.findById(id) ;
